e05b8cbb294951631c2a0ac6a91fd251468a5f6f
JuzzThyne <justinepodiotan19@gmail.com>
2023-12-20 21:26:24 +0800

    change text size

--- src/reusable-components/AlertComponent.jsx
+++ src/reusable-components/AlertComponent.jsx
@@ -11,7 +11,7 @@ const AlertComponent = ({ alertData }) => {
     <>
       {isAlertVisible && (
         <div className="bg-red-100 border border-red-400 mt-4 text-red-700 px-2 py-2 rounded relative" role="alert">
-          <span className="block sm:inline md:text-sm">{alertData}</span>
+          <span className="block sm:inline md:text-xs">{alertData}</span>
           <span className="absolute top-0 bottom-0 right-0 px-2 py-2" onClick={handleCloseClick} role="button">
             <svg className="fill-current h-6 w-6 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
               <title>Close</title>
