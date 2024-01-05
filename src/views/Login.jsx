e37a8811974c3e6b82e159c48b212e2bc04c8c0e
JuzzThyne <justinepodiotan19@gmail.com>
2024-01-05 17:26:54 +0800

    remove spinner in login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -40,7 +40,6 @@ const Login = () => {
     <div className="relative w-full h-screen md:w-80 md:h-96 mx-auto overflow-hidden md:overflow-visible">
       <div className="hidden md:block absolute w-70 h-70 md:w-40 md:h-40 bg-gradient-to-br from-blue-700 to-blue-500 rounded-full -left-40 -top-20 md:-left-20 md:-top-20"></div>
       <div className="hidden md:block absolute w-80 h-80 md:w-40 md:h-40 bg-gradient-to-r from-red-500 to-yellow-500 rounded-full -right-20 -bottom-[200px] "></div>
-      <ProgressBar/>
       {isLoading && <ProgressBar/>}
       <form onSubmit={handleLogin} className="absolute flex flex-col justify-center w-full h-screen md:w-80 md:h-[55vh] bg-opacity-10 bg-white bg-blur border-2 border-opacity-10 border-white md:rounded-2xl shadow-2xl p-10 overflow-hidden">
         <div className="flex justify-center items-center">
